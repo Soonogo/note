541ef2b78fe74d571ca081551a1793beeec7e5a6
songen <614750803@qq.com>
2023-07-16 01:14:03 +0800

    bundle rspec & add TDD

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_113630) do
   create_table "validation_codes", force: :cascade do |t|
     t.string "email"
     t.integer "kind", default: 0, null: false
+    t.string "code"
     t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
