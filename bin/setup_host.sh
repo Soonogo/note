74aa46968d907e7d84c7f6d0c7ef65944c15fcd9
songen <614750803@qq.com>
2023-07-18 01:59:45 +0800

    database env

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -10,5 +10,5 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -d -p 3000:3000 -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -d -p 3000:3000 -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
 echo 'DONE!'
