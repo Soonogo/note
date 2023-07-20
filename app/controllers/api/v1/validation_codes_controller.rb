19ea1300997807027c2a5007546ae81dcfdd16ea
songen <614750803@qq.com>
2023-07-21 01:44:26 +0800

    add email too many request handle

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
+    return render status:429 if ValidationCode.exists?(email:params[:email],kind:"sign_in",created_at:1.minute.ago..Time.now)
     validation_code = ValidationCode.new email:params[:email],kind:"sign_in"
     if validation_code.save
       render status:200
