d2ee34e665cdd97fe7b0d333253e768a4b24efeb
songen <614750803@qq.com>
2023-07-23 01:54:36 +0800

    add items api/doc

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::ItemsController < ApplicationController
   def index
     item = Item.where({created_at:params[:created_after]..params[:created_before]}).page params[:page]
     render json:{resource:item,pager:{
+      page:params[:page]||1,
+      per_page:Item.default_per_page,
       count:Item.count
     }}
   end
