cda0befb749e3183db571c61136c4b5b566e7c04
songen <614750803@qq.com>
2023-07-23 23:59:18 +0800

    finish tag controller

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::TagsController < ApplicationController
     def index
-        user_id = request.env['current_user_id']
-        tag = Tag.where(user_id:user_id).page(params[:page])
+        current_user = User.find_by_id request.env['current_user_id']
+        return render status: :unauthorized if current_user.nil?
+        tag = Tag.where(user_id:current_user.id).page(params[:page])
         render json:{resources:tag,pager:{
             page:params[:page]||1,
             per_page:Tag.default_per_page,
-            count:Tag.where(user_id:user_id).count
+            count:Tag.where(user_id:current_user.id).count
           }}
+    end
+    def create
+        current_user = User.find_by_id request.env['current_user_id']
+        return render status: :unauthorized if current_user.nil?
+        tag = Tag.new name:params[:name],sign:params[:sign],user_id:current_user.id
+        if tag.save
+            render json:{resource:tag},status:200
+        else
+            render json:{errors:tag.errors},status:422
+        end
+
     end
 end
--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Tags", type: :request do
   describe "tags" do
+    it "not sign_in" do
+      get '/api/v1/tags'
+      expect(response).to have_http_status(401)
+    end
     it "get tags" do
       user1 = User.create email:"tttsongen@foxmail.com"
       user2 = User.create email:"2@foxmail.com"
@@ -15,6 +19,33 @@ RSpec.describe "Tags", type: :request do
         expect(response).to have_http_status(200)
         expect(JSON.parse(response.body)["resources"].length).to eq(1)
     end
-   
+    it "get tags" do
+      user1 = User.create email:"tttsongen@foxmail.com"
+      user2 = User.create email:"2@foxmail.com"
+
+        11.times do |i| Tag.create name:"tag#{i}",sign:"x",user_id:user1.id end
+        11.times do |i| Tag.create name:"tag#{i}",sign:"x",user_id:user2.id end
+        get '/api/v1/tags',headers:user1.generate_jwt_header
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)["resources"].length).to eq(10)
+        get '/api/v1/tags',headers:user1.generate_jwt_header,params:{page:2}
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)["resources"].length).to eq(1)
+    end
+    it "post tags" do
+      user1 = User.create email:"tttsongen@foxmail.com"
+        post '/api/v1/tags',headers:user1.generate_jwt_header,params:{sign:"sign",name:"name"}
+        expect(response).to have_http_status(200)
+        json = JSON.parse response.body
+        expect(json["resource"]["sign"]).to eq("sign")
+        expect(json["resource"]["name"]).to eq("name")
+    end
+    it "post tags no sign field" do
+      user1 = User.create email:"tttsongen@foxmail.com"
+        post '/api/v1/tags',headers:user1.generate_jwt_header,params:{name:"name"}
+        expect(response).to have_http_status(422)
+        json = JSON.parse response.body
+        expect(json["errors"]["sign"][0]).to eq("can't be blank")
+    end
   end
 end
