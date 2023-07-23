bcbcb21c98ac153bbc840fbcb17db72b02e2e6fb
songen <614750803@qq.com>
2023-07-23 23:18:41 +0800

    perfect tags controller & api/doc

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::TagsController < ApplicationController
     def index
-        current_user = request.env['current_user_id']
-        tag = Tag.where(user_id:current_user).page(params[:page])
-        render json:{resources:tag}
+        user_id = request.env['current_user_id']
+        tag = Tag.where(user_id:user_id).page(params[:page])
+        render json:{resources:tag,pager:{
+            page:params[:page]||1,
+            per_page:Tag.default_per_page,
+            count:Tag.where(user_id:user_id).count
+          }}
     end
 end
